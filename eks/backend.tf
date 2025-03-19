73acdec604638c93d47b9c190081f6f9d90d755a
femithecoder <femithecoder@gmail.com>
2025-03-19 13:36:09 +0000

    new day

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_version = ">=0.12.0" 
+terraform {
+  required_version = ">=0.12.0" 
 
-#   backend "s3" {
-#     bucket = "capstone-s3-bucket-femi"
-#     key = "capstone/terraform.state"
-#     region = "us-east-1"
-#     dynamodb_table = "table"
-#     encrypt = false
-#     use_lockfile = true
-#   }
-# }
+  backend "s3" {
+    bucket = "capstone-s3-bucket-femi"
+    key = "capstone/terraform.state_1"
+    region = "us-east-1"
+    dynamodb_table = "table"
+    encrypt = false
+    use_lockfile = true
+  }
+}

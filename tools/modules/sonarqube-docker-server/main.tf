0927c296b67154b447e4c53c41a95abdb3fefdc0
femithecoder <femithecoder@gmail.com>
2025-05-04 17:32:59 +0100

    updated

--- tools/modules/sonarqube-docker-server/main.tf
+++ tools/modules/sonarqube-docker-server/main.tf
@@ -33,6 +33,12 @@ resource "aws_security_group" "slavenode1-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port = 0
     to_port = 0

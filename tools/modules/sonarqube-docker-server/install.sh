2fa1a8b1a79d8fbcf8f8c534f6fc4628563721c4
jidebola <jidedomaining@gmail.com>
2025-05-06 21:20:31 +0100

    another try

--- tools/modules/sonarqube-docker-server/install.sh
+++ tools/modules/sonarqube-docker-server/install.sh
@@ -163,3 +163,8 @@ sudo usermod -aG docker $USER
 
 echo "Docker installation completed successfully."
 echo "Please log out and log back in to apply group changes."
+
+#Installing kubectl client
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.12/2024-04-19/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH

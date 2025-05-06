f8decaf4a7ffb76d164538d7cb4ac9123fabd538
jidebola <jidedomaining@gmail.com>
2025-05-06 22:27:27 +0100

    kubectl added to sonar-node

--- tools/modules/sonarqube-docker-server/install.sh
+++ tools/modules/sonarqube-docker-server/install.sh
@@ -165,6 +165,8 @@ echo "Docker installation completed successfully."
 echo "Please log out and log back in to apply group changes."
 
 #Installing kubectl client
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.12/2024-04-19/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.3/2024-12-12/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.3/2024-12-12/bin/linux/amd64/kubectl.sha256
+sha256sum -c kubectl.sha256
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
